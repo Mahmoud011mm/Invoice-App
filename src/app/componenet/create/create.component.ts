d4f56605a8c9c8db9c65fc0ebfeb948bb37a7918
Mahmoud Turky <tourkeymahmoud@gmail.com>
2024-12-13 07:09:02 +0200

    Second Day

--- src/app/componenet/create/create.component.ts
+++ src/app/componenet/create/create.component.ts
@@ -1,21 +1,42 @@
-import { Component, Inject } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import {FormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {  MatDatepickerModule } from '@angular/material/datepicker';
+import { MatSelectModule } from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatListModule } from '@angular/material/list';
+import { Router } from '@angular/router';
+import { InvoiceService } from '../../service/invoice.service';
+import { Customer } from '../../model/customer';
+import { Product } from '../../model/product';
+import { Tax } from '../../model/tax';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [ReactiveFormsModule, MatCardModule, MatButtonModule],
+  providers: [provideNativeDateAdapter()],
+  imports: [ReactiveFormsModule, MatCardModule, MatButtonModule, MatInputModule, MatFormFieldModule, MatDatepickerModule,
+    MatSelectModule, MatIconModule,MatListModule
+  ],
   templateUrl: './create.component.html',
   styleUrl: './create.component.css'
 })
-export class CreateComponent {
-  title = "Create Invoice"
+export class CreateComponent implements OnDestroy, OnInit {
+  title = "Create Invoice";
+  customerList:Customer[]= [];
+  productList:Product[]= [];
+  taxList:Tax[]= [];
+  subscription = new Subscription()
+
   invoiceForm: any;
 
   // private builder = Inject(FormBuilder);
-  constructor( private builder: FormBuilder ){
+  constructor( private builder: FormBuilder, private router: Router, private service: InvoiceService ){
     this.invoiceForm = this.builder.group({
       invoiceno: this.builder.control({ value: '', disabled: true }),
       invoicedate: this.builder.control(new Date(), Validators.required),
@@ -29,8 +50,25 @@ export class CreateComponent {
       products: this.builder.array([])
     })
   }
+  ngOnInit(): void {
+    this.loadCustomer();
+  }
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  loadCustomer() {
+    let sub1 =this.service.getAllCustomers().subscribe( item => {
+      this.customerList = item
+    })
+    this.subscription.add(sub1)
+  }
 
 
   saveInvoice(){}
+
+  backToInvoce() {
+    this.router.navigateByUrl('/invoice')
+  }
 
 }
--- src/app/service/invoice.service.ts
+++ src/app/service/invoice.service.ts
@@ -1,9 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Customer } from '../model/customer';
+import { Tax } from '../model/tax';
+import { Product } from '../model/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InvoiceService {
 
-  constructor() { }
+  constructor( private http: HttpClient ) { }
+
+  getAllCustomers() {
+    return this.http.get<Customer[]>('http://localhost:3000/customer')
+  }
+  getCustomer(customerid: string) {
+    return this.http.get<Customer>('http://localhost:3000/customer/'+ customerid)
+  }
+  getAllTaxes() {
+    return this.http.get<Tax[]>('http://localhost:3000/tax')
+  }
+  getTax(taxcode: string) {
+    return this.http.get<Tax>('http://localhost:3000/tax/'+ taxcode)
+  }
+  getAllProducts() {
+    return this.http.get<Product[]>('http://localhost:3000/product')
+  }
+  getProduct(productcode: string) {
+    return this.http.get<Product>('http://localhost:3000/product/'+ productcode)
+  }
 }
